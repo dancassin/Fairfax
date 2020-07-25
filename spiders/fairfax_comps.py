5af9c18c17ca43bc9d043c69ee22c3c8c3d3dba0
Dan Cassin <dancassin@gmail.com>
2020-07-25 10:12:54 -0700

    updated jupyter notebook

--- spiders/fairfax_comps.py
+++ spiders/fairfax_comps.py
@@ -28,25 +28,27 @@ class FairfaxSpider(scrapy.Spider):
         options.add_argument(" -–disable-web-security")
         options.add_argument("--no-sandbox") 		
         caps = options.to_capabilities()
+        
         streetNumber = str(input('Please type in house number: '))
         streetName = str(input('Please type in street: '))
+
         self.driver = webdriver.Chrome('/Users/DanCassin/Development/scrapy_projects/chromedriver')
         self.driver.get('https://icare.fairfaxcounty.gov/ffxcare/search/commonsearch.aspx?mode=address')
         search_address = self.driver.find_element_by_xpath('//*[(@id = "inpNumber")]').send_keys(streetNumber)
         search_street = self.driver.find_element_by_xpath('//*[(@id = "inpStreet")]').send_keys(streetName + '\ue007')
         sleep(2)
 
-        # comp_selector = Selector(text = self.driver.page_source)
-        # comp_page = self.driver.find_element_by_css_selector('#DTLNavigator_lbNbhdSearchThis span')
-        # comp_page.click()
+        comp_selector = Selector(text = self.driver.page_source)
+        comp_page = self.driver.find_element_by_css_selector('#DTLNavigator_lbNbhdSearchThis span')
+        comp_page.click()
 
-        # page_total_selector = Selector(text = self.driver.page_source)
-        # total_pages = ''.join(page_total_selector.css('#ml+ b::text').extract())
-        # total_pages = int(''.join([i for i in total_pages if i.isdigit()]))
+        page_total_selector = Selector(text = self.driver.page_source)
+        total_pages = ''.join(page_total_selector.css('#ml+ b::text').extract())
+        total_pages = int(''.join([i for i in total_pages if i.isdigit()]))
 
-        # first_result = self.driver.find_element_by_css_selector('.SearchResults:nth-child(3) td:nth-child(1) div')
-        # first_result.click()
-        # sleep(2)
+        first_result = self.driver.find_element_by_css_selector('.SearchResults:nth-child(3) td:nth-child(1) div')
+        first_result.click()
+        sleep(2)
 
         while FairfaxSpider.current_page <= 1:#total_pages:
             profile_selector = Selector(text = self.driver.page_source)
@@ -103,13 +105,13 @@ class FairfaxSpider(scrapy.Spider):
             sleep(0.5)
 
             #profile page at bottom so it stays as the .sel class for the next page
-            # profile_page = self.driver.find_element_by_css_selector('#sidemenu .unsel:nth-child(1) span')
-            # profile_page.click()
-            # sleep(2)
+            profile_page = self.driver.find_element_by_css_selector('#sidemenu .unsel:nth-child(1) span')
+            profile_page.click()
+            sleep(2)
 
-            # next_button = self.driver.find_element_by_css_selector('#DTLNavigator_imageNext')
-            # next_button.click()
-            # sleep(1)
+            next_button = self.driver.find_element_by_css_selector('#DTLNavigator_imageNext')
+            next_button.click()
+            sleep(1)
 
 
             items['address'] = address
